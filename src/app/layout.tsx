5a74e16d098bd72a82f15afc505bf7544a9fbdfa
acevnn <ninoacev@gmail.com>
2026-02-28 16:19:06 +0100

    feature/streaming-platform-001-initialize-project-and-setup - favicon update, initial code cleanup, new font 'inter' used, mixins created for responsive app, variables file added, SASS & prettier libraries added

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "../styles/globals.scss";
+import React from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Streamform",
+  description: "Sports and entertainment streaming platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.variable}>{children}</body>
+    </html>
+  );
+}
